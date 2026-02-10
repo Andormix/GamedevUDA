5ae4879392382aed17039ac786b7a5cb40bc0069
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-02-10 19:14:09 +0100

    Added Arrow Keys + Old WASD for Movement
    WASD + Arrow Keys for Movement

--- Assets/Scripts/Input Scripts/GameInput.cs
+++ Assets/Scripts/Input Scripts/GameInput.cs
@@ -14,7 +14,7 @@ public class GameInput : MonoBehaviour
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
-        Debug.Log(inputVector);
+        //Debug.Log(inputVector);
         return inputVector;
     }
 }
