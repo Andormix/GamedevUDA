6853106e1903e8e8a72d0d5e39becb445453b1a3
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-02-10 19:04:47 +0100

    Refractor to new Input System
    Created and set an InputActions
    Refractored to new input System

--- Assets/Scripts/Input Scripts/GameInput.cs
+++ Assets/Scripts/Input Scripts/GameInput.cs
@@ -2,29 +2,19 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions  = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNorm()
     {
-        Vector2 inputVector = new Vector2(0,0);
-
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        // Normalizamos para que el movimiento en diagonal x+y sea igual a 1 y no superior.
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
+        Debug.Log(inputVector);
         return inputVector;
     }
 }
