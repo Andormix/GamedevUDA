a993bbb796530f0d77b859dbb62a6a3258b5fa2f
Eric Torrontera Ruiz <eric@aNDORRA.AD>
2026-02-10 18:35:59 +0100

    Decoupled the input from the player class.
    Decoupled the input from the player class.

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -4,34 +4,16 @@ public class Player : MonoBehaviour
 {
     //VARS
     [SerializeField] private float movementsSpeed = 5f; //Variables publicas permiten editar desde el editor de Unity pero cualquier clase puede acceder.
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
 
     // Update is called once per frame
     private void Update()
     {
-        //Lógica del input y movimiento separadas.
-        Vector2 inputVector = new Vector2(0,0);
+        //Separación de lógicas
+        Vector2 inputVector = gameInput.GetMovementVectorNorm();
+
         Vector3 movementDirection;
-
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        // Normalizamos para que el movimiento en diagonal x+y sea igual a 1 y no superior.
-        inputVector = inputVector.normalized;
         movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += movementDirection * movementsSpeed * Time.deltaTime;  //Time.deltatime para que la velociadad sea independiente del framerate.
 
